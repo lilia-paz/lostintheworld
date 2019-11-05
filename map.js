7c6669608e8205c9489c44f54e3d24902c6b63e2
Lilia Paz <lpla1154@gmail.com>
2019-11-05 10:12:50 -0800

    adding to list of zipcodes

--- map.js
+++ map.js
@@ -14,12 +14,42 @@ $.ajax({
       this.latitude = latitude;
       this.longitude = longitude;
     }
-  
-  var p = new Point(37.797557,-122.4012137);
-  var r = new Point(37.77245,-122.4119627);
+  var a = new Point(37.780350, -122.415748);
+  var b = new Point(37.77245,-122.4119627);
+  var c = new Point();
+  var d = new Point();
+  var e = new Point();
+  var i = new Point(37.797557,-122.4012137);
   var 
   
   var myMap = new Map();
-  myMap.set(94111, p);
-  myMap.set(94103, r);
+  myMap.set(94102, a);
+  myMap.set(94103, b);
+  myMap.set(94104, c);
+  myMap.set(94105, d);
+  myMap.set(94107, e);
+  myMap.set(94108, f);
+  myMap.set(94109, g);
+  myMap.set(94110, h);
+  myMap.set(94111, i);
+  myMap.set(94112, j);
+  myMap.set(94114, k);
+  myMap.set(94115, l);
+  myMap.set(94116, m);
+  myMap.set(94117, n);
+  myMap.set(94118, o);
+  myMap.set(94121, p);
+  myMap.set(94122, q);
+  myMap.set(94123, r);
+  myMap.set(94124, s);
+  myMap.set(94127, t);
+  myMap.set(94129, u);
+  myMap.set(94130, v);
+  myMap.set(94131, w);
+  myMap.set(94132, x);
+  myMap.set(94133, y);
+  myMap.set(94134, z);
+  myMap.set(94158, zz);
+  
+  
   
