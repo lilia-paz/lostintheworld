7d051b0259ccbd0656407228c659321cbc3606d9
Lilia Paz <lpla1154@gmail.com>
2019-11-07 14:26:10 -0800

    clearing code

--- maths.js
+++ maths.js
@@ -1,17 +1,16 @@
 // let lat1;
-        // let lat2;
-        // let lon1;
-        // let lon2;
-        // var R = 3958.8; // miles
-        // var alpha = lat1.toRadians();
-        // var beta = lat2.toRadians();
-        // var omega = (lat2-lat1).toRadians();
-        // var tau = (lon2-lon1).toRadians();
+// let lat2;
+// let lon1;
+// let lon2;
+// var R = 3958.8; // miles
+// var alpha = lat1.toRadians();
+// var beta = lat2.toRadians();
+// var omega = (lat2-lat1).toRadians();
+// var tau = (lon2-lon1).toRadians();
 
-        // var a = Math.sin(omega/2) * Math.sin(omega/2) +
-        //         Math.cos(alpha) * Math.cos(beta) *
-        //         Math.sin(tau/2) * Math.sin(tau/2);
-        // var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+// var a = Math.sin(omega/2) * Math.sin(omega/2) +
+//         Math.cos(alpha) * Math.cos(beta) *
+//         Math.sin(tau/2) * Math.sin(tau/2);
+// var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-        // var d = R * c;
-
+// var d = R * c;
