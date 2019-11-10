2929b5a2e6e61883d3f8b21c0965defc857f0ef1
Lilia Paz <lpla1154@gmail.com>
2019-11-10 11:04:42 -0800

    refactoring db

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+import { BADSTR } from "dns"
+
 $.ajax({
   url: 'https://data.sfgov.org/resource/cf6e-9e4j.json',
   type: 'GET',
@@ -73,4 +75,133 @@ myMap.set(94158, zz)
 
 function findLoc(zipNum){
     return myMap.get(zipNum)
+}
+//i get a point and pass to function below: (point1)
+function calcDistanceBetween(lat1, lon1, lat2, lon2) {//or params can be point 1, point 2
+  //Radius of the earth in:  1.609344 miles,  6371 km  | var R = (6371 / 1.609344);
+  var R = 3958; // Radius of the earth in km
+  var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+  var dLon = (lon2 - lon1) * Math.PI / 180;
+  var a = 
+     0.5 - Math.cos(dLat)/2 + 
+     Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
+     (1 - Math.cos(dLon))/2;
+
+  return R * 2 * Math.asin(Math.sqrt(a));
+}
+// let lat1;
+// let lat2;
+// let lon1;
+// let lon2;
+// var R = 3958.8; // miles
+// var alpha = lat1.toRadians();
+// var beta = lat2.toRadians();
+// var omega = (lat2-lat1).toRadians();
+// var tau = (lon2-lon1).toRadians();
+
+// var a = Math.sin(omega/2) * Math.sin(omega/2) +
+//         Math.cos(alpha) * Math.cos(beta) *
+//         Math.sin(tau/2) * Math.sin(tau/2);
+// var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+// var d = R * c;
+
+
+// every data point in the object.point should be iterated thru the formula. this distance should then be stored in an array.
+// Everything that is returned from here needs to be stored in an array.
+// this should be returned in miles.
+// var d = R * c;
+// need an array of the 65 artworks.
+// for loop that will iterate thru the haversine array.
+//   declare an empty variable to store results.
+//   if a distance is greater than 2, take the result and store in a variable.
+//   finish loop
+//   return the indices of the artworks [different array]
+
+
+// // FUNCTIONS 
+// const zipNum = req.query.name
+// function findLoc (zipNum) {
+//   return myMap.get(zipNum)
+// }
+
+// function calcDistanceBetween (lat1, lon1, lat2, lon2) {
+//   var R = 3958 // Radius of the earth in km
+//   var dLat = (lat2 - lat1) * Math.PI / 180 // deg2rad below
+//   var dLon = (lon2 - lon1) * Math.PI / 180
+//   var a =
+//      0.5 - Math.cos(dLat) / 2 +
+//      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+//      (1 - Math.cos(dLon)) / 2
+
+//   return R * 2 * Math.asin(Math.sqrt(a))
+// }
+
+// function iterateTheDistance () {
+//   var distanceBetweenPoints = calcDistanceBetween(); 
+//   var distanceBetweenUserAndFirst = calcDistanceBetween(point1.latitude, point1.longitude, Data[0].the_geom.coordinates[0], Data[0].the_geom.coordinates[1])
+
+//   for (let i = 0; i < 65; i++) {
+
+//     let  distancesCalculated = calcDistanceBetween(point1.latitude, point1.longitude, Data[i].the_geom.coordinates[0], Data[0].the_geom.coordinates[1])
+//     distancesCalculated.push()
+//   } //if it's less than two, store that in a variable,
+
+// // return those indices. 
+// }
+  // point1.latitude 
+  // point1.longitude
+  // calcDistanceBetween(point1.latitude, point1.longitude,  )
+  // var distanceBetweenPoints = calcDistanceBetween(); 
+  // var distanceBetweenUserAndFirst = calcDistanceBetween(point1.latitude, point1.longitude, Data[0].the_geom.coordinates[0], Data[0].the_geom.coordinates[1])
+
+
+  function findFive(array){
+    let emptyArr = [];
+    for (let i = 0; i < array.length, i++){
+      if (array[i] < 5){
+        emptyArr.push(array[i]);
+      }
     }
+    return emptyArr; 
+  }
+  function findKinder(array){
+    let emptyArr = [];
+    for (let i = 0; i < array.length; i ++){
+
+      if (array[i].age < 5){
+        emptyArr.push(array[i]);
+      }
+    }
+    return emptyArr; 
+  }
+
+let userCoordinates = function findLoc(zipNum){
+    return myMap.get(zipNum)
+}
+
+function calcDistanceBetween(lat1, lon1, lat2, lon2) {//or params can be point 1, point 2
+    //Radius of the earth in:  1.609344 miles,  6371 km  | var R = (6371 / 1.609344);
+    var R = 3958; // Radius of the earth in km
+    var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a = 
+       0.5 - Math.cos(dLat)/2 + 
+       Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
+       (1 - Math.cos(dLon))/2;
+  
+    return R * 2 * Math.asin(Math.sqrt(a));
+
+function findArtwork(userZip, array){
+  let resultArr = [];
+  let userCoordinates = function findLoc(userZip){
+    return myMap.get(userZip)
+  }
+  for (let i = 0; i < array.length; i++){
+    let resultDistance = function calcDistanceBetween(array[i].the_geom.coordinates[0], array[i].the_geom.coordinates[1], userCoordinates);
+    if (resultDistance < 2){
+      resultArr.push(array[i])
+      }
+    }
+    return resultArr;
+  }
