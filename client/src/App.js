8e22eb1e973f779f4ca055d5779af73018de86f7
Lilia Paz <lpla1154@gmail.com>
2019-11-17 21:12:50 -0800

    debugging for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import ExternalApi from "./components/ExternalApi";
+import NavBar from './components/NavBar';
+import PrivateRoute from './components/PrivateRoute';
+import Profile from './components/Profile';
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path='/' exact />
+          <PrivateRoute path='/profile' component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
