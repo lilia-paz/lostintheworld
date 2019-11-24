a7b9b645ac178f0b761ac43b8d703cda901fe04c
Lilia Paz <lpla1154@gmail.com>
2019-11-24 11:35:20 -0800

    cleaning up code

--- server/server.js
+++ server/server.js
@@ -1,35 +1,53 @@
 // require('dotenv').config();
 const express = require('express')
-var hbs = require('express-handlebars')
-const app = express()
-var Sqrl = require('squirrelly')
-const path = require('path')
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+// Create a new Express app
+const app = express();
+const mongoose = require('mongoose');
+var cors = require('cors');
 var bodyParser = require('body-parser')
-var xmlHttpRequest = require('xmlhttprequest').XMLHttpRequest
-var xmlHttp = new xmlHttpRequest()
+const API_PORT = 5000;
+app.use(cors());
+const router = express.Router();
 
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//   user: 'tpl619_6',
-//   host: 'localhost',
-//   database: 'project1',
-//   password: 'lpaz',
-//   port: 5432
-// })
+const authConfig = {
+  domain: "dev-ibrtdcrj.auth0.com",
+  audience: "YOUR_API_IDENTIFIER"
+};
+// Define middleware that validates incoming bearer tokens
+// using JWKS from dev-ibrtdcrj.auth0.com
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-ibrtdcrj.auth0.com/.well-known/jwks.json'
+  }),
 
-function createFavorite () {
-  const places_id = '66 9th'
-  console.log(places_id)
-  const user_id = 456
-  pool.query('INSERT INTO favorites (places_id, user_id) VALUES ($1, $2)', [places_id, user_id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    console.log(`User saved ID: ${results.insertId}`)
-  })
-}
-// document.
-// document.getElementById("myBtn").addEventListener("click", createFavorite);
+  audience: 'https://api.mysite.com',
+  issuer: 'https://dev-ibrtdcrj.auth0.com/',
+  algorithms: ['RS256']
+});
+
+// database 
+
+// this is our MongoDB database
+const dbRoute ='mongodb+srv://liliapaz:y0ungthug@cluster0-3puql.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+db.once('open', () => console.log('connected to the database'));
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!"
+  });
+});
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -42,3 +60,6 @@ app.use('/', require('./routes/api'))
 
 // listening at port
 app.listen(app.get('port'), () => console.log(`My server is running ${app.get('port')}...`))
+
+// Start the app
+// app.listen(3001, () => console.log('API listening on 3001'));
