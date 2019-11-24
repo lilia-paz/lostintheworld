1fe276944e1f2a2f443000fe5b8864014b230295
Lilia Paz <lpla1154@gmail.com>
2019-11-24 12:42:02 -0800

    consolidating components

---
+++ client-final/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>  <p class="font-weight-light"> Welcome! Log in to begin your journey.</p> </div>
+  )
+}
+
+export default Home
