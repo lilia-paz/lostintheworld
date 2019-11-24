e6796af6e272f9ce4360a03bd21004a23519320f
Lilia Paz <lpla1154@gmail.com>
2019-11-24 12:43:50 -0800

    consolidating components

---
+++ client-final/src/components/Twitter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {  TwitterShareButton, TwitterHashtagButton } from 'react-twitter-embed';
+
+const Twitter = () => {
+  return (
+    <TwitterHashtagButton
+      tag={'lostintheworld'}
+    />
+
+  )
+}
+
+export default Twitter
